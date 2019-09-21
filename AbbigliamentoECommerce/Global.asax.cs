d940425e2ae558933c327fc3296a2c001cccc160
Federica Arpaia <federigna@gmail.com>
2019-09-21 11:28:26 +0200

    Gestione Log

--- AbbigliamentoECommerce/Global.asax.cs
+++ AbbigliamentoECommerce/Global.asax.cs
@@ -16,6 +16,8 @@ namespace AbbigliamentoECommerce
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
