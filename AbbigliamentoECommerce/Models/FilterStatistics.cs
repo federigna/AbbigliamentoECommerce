0602d77be8a20b64d0cf32c29bccaf20f37f2841
Adina Barresi <adinabarresiIngSoft@gmail.com>
2019-09-24 14:31:38 +0200

    Creazione Filtri Statistiche

---
+++ AbbigliamentoECommerce/Models/FilterStatistics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AbbigliamentoECommerce.Models
+{
+    public class FilterStatistics : BaseClass
+    {
+        
+        [Display(Name = "Data Inizio")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indicare la data di inizio")]
+        public DateTime Datastart { get; set; }
+        [Display(Name = "Data Fine")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indicare la data di inizio")]
+        public DateTime DataEnd { get; set; }
+        [Display(Name = "Categoria")]
+        public List<Category> Categories { get; set; }
+        [Display(Name = "Categoria")]
+        public string Category { get; set; }
+
+        public string NameStatistic { get; set; }
+
+    }
+}
