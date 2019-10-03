ba766613c3a7d1369ae7006cbb495d6fd3a02492
Adina Barresi <adinabarresiIngSoft@gmail.com>
2019-10-03 12:25:19 +0200

    Statistiche DB

---
+++ AbbigliamentoECommerce/Models/FilterStatistics2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbbigliamentoECommerce.Models
+{
+    public class FilterStatistics2 : BaseClass
+    {
+
+        [Display(Name = "Data Inizio")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indicare la data di inizio")]
+        public DateTime Datastart { get; set; }
+        [Display(Name = "Data Fine")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indicare la data di inizio")]
+        public DateTime DataEnd { get; set; }
+        public string NameStatistic { get; set; }
+        public FileContentResult Chart { get; set; }
+
+    }
+}
